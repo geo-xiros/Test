3be401cbdd3ff6d3efd301af6f55bb6bfc00298d
george <geo.xiros@gmail.com>
2018-11-22 13:12:39 +0200

    Fix Roles and User Access

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,25 +10,29 @@ namespace Test
         static void Main(string[] args)
         {
             Database db = new Database();
-            Console.WriteLine(db.Login("george", "1234"));
-            Console.WriteLine(db.Login("admin", "admin"));
-            Console.WriteLine(db.Login("nick", "1234"));
             Console.ReadKey();
         }
     }
     class Database
     {
         private Dictionary<string, User> _users;
-        private Dictionary<string, Role> _roles;
         public Database()
         {
             _users = new Dictionary<string, User>();
-            _roles = new Dictionary<string, Role>();
-            _roles.Add("user", new Role("user", true, true, false, false));
-            _roles.Add("admin", new Role("admin", true, true, false, false));
 
-            _users.Add("admin", new User(1, "admin", "admin", _roles["admin"]));
-            _users.Add("george", new User(2, "george", "1234", _roles["user"]));
+            _users.Add("admin", new AdminUser());
+            _users.Add("george", new SimpleUser(1, "george", "1234", new ViewRole()));
+            _users.Add("nick", new SimpleUser(2, "nick", "1234", new ViewEditRole()));
+            _users.Add("john", new SimpleUser(3, "john", "1234", new ViewEditDeleteRole()));
+
+            foreach (var un in _users)
+            {
+                Console.WriteLine(un.Value.Username);
+                Console.WriteLine(Responsibilities.ViewData + " " + un.Value.HasAccessTo(Responsibilities.ViewData));
+                Console.WriteLine(Responsibilities.EditData + " " + un.Value.HasAccessTo(Responsibilities.EditData));
+                Console.WriteLine(Responsibilities.DeleteData + " " + un.Value.HasAccessTo(Responsibilities.DeleteData));
+            }
+
         }
         public bool Login(string username, string password)
         {
@@ -37,39 +41,67 @@ namespace Test
         }
 
     }
+    public enum Responsibilities { ViewData, EditData, DeleteData };
     class Role
     {
-        public readonly string Name;
-        public readonly bool EditAccess;
-        public readonly bool AddAccess;
-        public readonly bool DeleteAccess;
-        public readonly bool ViewAccess;
-        public Role(string name, bool viewAccess, bool addAccess, bool editAccess, bool deleteAccess)
+        private Responsibilities[] _responsibilities;
+        protected Role(Responsibilities[] responsibilities)
         {
-            Name = name;
-            EditAccess = editAccess;
-            DeleteAccess = deleteAccess;
-            ViewAccess = viewAccess;
-            AddAccess = addAccess;
+            _responsibilities = responsibilities;
         }
 
+        public bool this[Responsibilities responsibility]
+        {
+            get
+            {
+                return _responsibilities.Contains(responsibility);
+            }
+        }
     }
+    class ViewRole : Role
+    {
+        public ViewRole() : base(new Responsibilities[] { Responsibilities.ViewData }) { }
+    }
+    class ViewEditRole : Role
+    {
+        public ViewEditRole() : base(new Responsibilities[] { Responsibilities.ViewData, Responsibilities.EditData }) { }
+    }
+    class ViewEditDeleteRole : Role
+    {
+        public ViewEditDeleteRole() : base(new Responsibilities[] { Responsibilities.ViewData, Responsibilities.EditData, Responsibilities.DeleteData }) { }
+    }
+
     class User
     {
         private readonly int _nameId;
-        public readonly string Name;
+        public readonly string Username;
         private readonly string _password;
-        private readonly Role _role;
-        public User(int nameId, string name, string password, Role role)
+
+        protected User(int nameId, string name, string password)
         {
             _nameId = nameId;
-            Name = name;
+            Username = name;
             _password = password;
+        }
+        public bool IsPasswordCorrect(string password) { return _password == password; }
+        public virtual bool HasAccessTo(Responsibilities responsibility) { return true; }
+
+    }
+    class SimpleUser : User
+    {
+        private readonly Role _role;
+        public SimpleUser(int nameId, string name, string password, Role role)
+            : base(nameId, name, password)
+        {
             _role = role;
         }
-        public bool IsPasswordCorrect(string password)
+        public override bool HasAccessTo(Responsibilities responsibility)
         {
-            return _password == password;
+            return _role[responsibility];
         }
+    }
+    class AdminUser : User
+    {
+        public AdminUser() : base(0, "admin", "admin") { }
     }
 }
